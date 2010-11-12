a7dc2d40e9ecd92253d484ae18cfeb4d938b32a8
Cristian Fuentes Aguirre <cfuentesa98@gmail.com>
2010-11-12 17:45:50 -0300

    Mi segunda Clase
    Signed-off-by: Cristian Fuentes Aguirre <cfuentesa98@gmail.com>

---
+++ src/MyClass.cpp
@@ -0,0 +1,17 @@
+/*
+ * MyClass.cpp
+ *
+ *  Created on: 12-11-2010
+ *      Author: cfuentesa
+ */
+
+#include "MyClass.h"
+
+MyClass::MyClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MyClass::~MyClass() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/MyClass.h
@@ -0,0 +1,17 @@
+/*
+ * MyClass.h
+ *
+ *  Created on: 12-11-2010
+ *      Author: cfuentesa
+ */
+
+#ifndef MYCLASS_H_
+#define MYCLASS_H_
+
+class MyClass {
+public:
+	MyClass();
+	virtual ~MyClass();
+};
+
+#endif /* MYCLASS_H_ */
